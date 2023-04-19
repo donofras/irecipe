95722b6d5a64c19be7e92058660618207c7e9a9a
Denis Onofras <denis.o@gardy.me>
2023-04-19 08:49:50 +0300

    initial commit

---
+++ iRecipe/iRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRecipeApp.swift
+//  iRecipe
+//
+//  Created by Denis Onofras on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct iRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
